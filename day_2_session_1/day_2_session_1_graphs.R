b7c098b0fc5c2a3e7823972ce13eb98e861f7b19
Brian Williamson <brianw26@uw.edu>
2017-08-22 11:45:28 -0700

    Added FEV data; updated R file, plots, and slides accordingly

--- day_2_session_1/day_2_session_1_graphs.R
+++ day_2_session_1/day_2_session_1_graphs.R
@@ -16,46 +16,55 @@
 ## MMDDYY INIT COMMENTS
 ## ------ ---- --------
 ## 220817 BDW  Set new working directory; remove line example; motivate all sections
-##             in slides using leprosy data
+##             in slides using FEV data, not leprosy data
 #################################################################################
 
 ## set working directory, just to be sure
 setwd("day_2_session_1")
 
 ## create a histogram, boxplot, scatterplot
-## load in some sample data
-leprosy <- read.table("data/leprosy.txt", header = TRUE)
+## load in some sample data (from Scott Emerson, at https://www.emersonstatistics.com/datasets/fev.txt)
+fevdat <- read.table("data/fev.txt", header = TRUE)
 
-## histogram of count1
+## clean up the data, based on documentation (https://www.emersonstatistics.com/datasets/fev.doc)
+fevdat$smoke <- (-1)*(fevdat$smoke - 2)
+fevdat$female <- fevdat$sex - 1
+
+## histogram of fev
 png("figs/day_2_graphs_hist_example.png", width = 400, height = 300)
 par(mar=c(5.1, 4.1, 4.1, 0.1), cex.lab = 1.45)
-hist(leprosy$count1, main = "", xlab = "Baseline counts of leprosy bacilli", ylab = "Frequency")
+hist(fevdat$fev, main = "", xlab = "FEV", ylab = "Frequency")
 dev.off()
 
 ## boxplot of count1 by severe or not
 png("figs/day_2_graphs_bplot_example.png", width = 400, height = 300)
 par(mar=c(5.1, 4.1, 4.1, 0.1), cex.lab = 1.5, cex.axis = 1.25)
-boxplot(leprosy$count1 ~ leprosy$severe, xlab = "Severity", ylab = "Baseline counts of bacilli", axes = FALSE)
-axis(side = 2, at = seq(0, max(leprosy$count1) + 5, by = 5))
-axis(side = 1, at = c(1, 2), labels = c("Non-severe", "Severe"))
+boxplot(fevdat$fev ~ fevdat$smoke, xlab = "Smoking status", ylab = "FEV", axes = FALSE)
+axis(side = 2, at = seq(0, max(fevdat$fev) + 5, by = 1))
+axis(side = 1, at = c(1, 2), labels = c("Non-smoker", "Smoker"))
 box()
 dev.off()
 
-## scatterplot of count1 vs count2, colored by treatment group
-sub.1 <- subset(leprosy, leprosy$trt == 1)
-sub.2 <- subset(leprosy, leprosy$trt == 2)
-sub.3 <- subset(leprosy, leprosy$trt == 3)
+## scatterplot of fev vs age, colored by sex
+sub.1 <- subset(fevdat, fevdat$female == 0)
+sub.2 <- subset(fevdat, fevdat$female == 1)
+
+## jitter the data on the x-axis to make it a bit easier to see
+set.seed(4747)
+sub.1.plotdata <- sub.1
+sub.1.plotdata$age <- sub.1$age + runif(length(sub.1$age), -0.05, 0.05)
+set.seed(5555)
+sub.2.plotdata <- sub.2
+sub.2.plotdata$age <- sub.2$age + runif(length(sub.2$age), -0.05, 0.05)
 
 png("figs/day_2_graphs_scatplot_example.png", width = 400, height = 300)
 par(mar = c(5.1, 4.1, 4.1, 0.1), cex.lab = 1.5)
-plot(sub.1$count1, sub.1$count2, main = "", ylab = "Count of bacilli at time 1", xlab = "Baseline count of bacilli",
-     col = "black", pch = 19, xlim = c(min(leprosy$count1), max(leprosy$count1)),
-     ylim = c(min(leprosy$count2), max(leprosy$count2)))
-points(sub.2$count1, sub.2$count2, pch = 18, col = "blue")
-points(sub.3$count1, sub.3$count2, pch = 17, col = "red")
-legend(2.5, 23, legend = c("Treatment 1", "Treatment 2", "Treatment 3"), col = c("black", "blue", "red"),
-       pch = c(19, 18, 17), bty = "n", cex = 1.25)
-lines(sub.1$count1, fitted(lm(sub.1$count2 ~ sub.1$count1)))
-lines(sub.2$count1, fitted(lm(sub.2$count2 ~ sub.2$count1)), col = "blue")
-lines(sub.3$count1, fitted(lm(sub.3$count2 ~ sub.3$count1)), col = "red")
+plot(sub.1.plotdata$age, sub.1$fev, main = "", ylab = "FEV", xlab = "Age",
+     col = "black", pch = 19, xlim = c(min(fevdat$age), max(fevdat$age)),
+     ylim = c(min(fevdat$fev), max(fevdat$fev)))
+points(sub.2.plotdata$age, sub.2$fev, pch = 18, col = "blue")
+legend("topleft", legend = c("Male", "Female"), col = c("black", "blue"),
+       pch = c(19, 18), cex = 1.25)
+lines(sub.1$age, fitted(lm(sub.1$fev ~ sub.1$age)))
+lines(sub.2$age, fitted(lm(sub.2$fev ~ sub.2$age)), col = "blue")
 dev.off()
